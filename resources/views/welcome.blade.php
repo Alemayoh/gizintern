5a1b428ae0fb7ce0c258274f08f9f4331e1a5a3e
Alex Solomon <solomonalex592@gmail.com>
2018-07-10 17:52:41 +0300

    Story 182: Fix benchmarks for speed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                  Alex----> Hello Laravel GIZ
                 </div>
 
                 <div class="links">
